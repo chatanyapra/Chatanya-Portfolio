182abd0fa093b7e4a4776fb2e521faa744cb2ab2
chatanyapra <chatanyapra@gmail.com>
2023-11-14 08:49:27 -0800

    update the project components in this file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import GreetingPage from './components/GreetingPage'
 import Navbar from './components/Navbar'
 import SvgLine from "./components/SvgLine";
+import WorkPage from './components/WorkPage';
 
 function App() {
 
@@ -10,6 +11,14 @@ function App() {
       <SvgLine></SvgLine>
       <Navbar></Navbar>
       <GreetingPage></GreetingPage>
+      <div className='w-full justify-evenly flex flex-wrap relative m-auto p-16 bg-gray-900'>
+        <WorkPage></WorkPage>
+        <WorkPage></WorkPage>
+        <WorkPage></WorkPage>
+        <WorkPage></WorkPage>
+        <WorkPage></WorkPage>
+        <WorkPage></WorkPage>
+      </div>  
     </div>
   )
 }
