6c3c46342564fabb7a9581e913cd429c77376fda
chatanyapra <chatanyapra@gmail.com>
2023-11-14 08:51:27 -0800

    change about me section and image

--- src/components/GreetingPage.jsx
+++ src/components/GreetingPage.jsx
@@ -19,12 +19,10 @@ export default function GreetingPage() {
   const ImageMove= useRef(null);
   const box2Container= useRef(null);
   const [scrollPosition, setScrollPosition] = useState(0);
-  // const [count, setCount] = useState(0);
 
   useEffect(() => {
     const boxContainer= box2Container.current;
     const boxDist= boxContainer.getBoundingClientRect();
-    console.log(boxDist.top );
     const handleScroll = () => {
 
       setScrollPosition(()=>{
@@ -46,14 +44,14 @@ export default function GreetingPage() {
 
   const imageStyle = {
     transform: `translateX(-${scrollPosition}px) scaleX(-1)`,
-    transition: 'transform 0.5s ease-out',
+    transition: 'transform 0.5s ease-out', right: `${-1038}px`
   };
   const gearBox={
     transform: `rotate(${scrollPosition * 0.3}deg)`
   };
   return (
-    <div style={{height: '2500px'}}>
-      <div className='image-main flex space-around bg-gray-900'>
+    <div style={{height: '1510px'}} className='bg-gray-900'>
+      <div className='image-main flex space-around'>
           <div className="image-robot-container">
             <Lottie className='robot-anim' animationData={robotAnimation} loop={true} />
           </div>
@@ -62,11 +60,10 @@ export default function GreetingPage() {
                 <Cursor cursorColor='white' cursorStyle= {symbol} />
           </div>
       </div>
-      <div ref={box2Container} className="box2-container overflow-hidden absolute w-full bg-gray-900" style={{height: '700px', top: '712px'}}>
-          <div className='aboutme-box text-white bg-slate-600 rounded-2xl relative backdrop-opacity-10 backdrop-invert bg-white/30 py-7 overflow-x-hidden'>
+      <div ref={box2Container} className="box2-container overflow-hidden absolute w-full bg-gray-900" style={{height: '900px', top: '710px'}}>
+          <div className='aboutme-box text-white bg-slate-600 rounded-2xl relative backdrop-opacity-5 z-10 backdrop-invert bg-black/50 py-7 overflow-x-hidden'>
             <i style={gearBox} className="gear-icon absolute fa-solid fa-gear"></i>
             <h1 className='text-3xl mx-12 my-6'>About me</h1>
-            {/* <span className='span-line absolute bg-teal-300'></span> */}
             <div className='line-main absolute bg-teal-300'>
               <div className='scanner relative bg-white'></div>
             </div>
@@ -75,9 +72,11 @@ export default function GreetingPage() {
             </div>
             <h1 className='text-2xl mx-12 mt-10 mb-4'>My Journey</h1>
             <div className='m-auto text-xl w-10/12'>
-              Driven by a passion for problem-solving, I embarked on my journey in web development with a focus on crafting seamless and visually appealing solutions. From the intricacies of frontend design to the robustness of backend development, I have honed my skills to deliver results that exceed expectations.
+              <b>Innovative Solutions: </b>I approach challenges with a mindset that values innovation. Each project is an opportunity to create something unique and impactful, and I thrive on pushing the boundaries of what's possible.  
             </div>
-      </div>
+            <div className='m-auto text-xl pt-5 w-10/12'>
+              <b>Continuous Learning: </b> The ever-evolving nature of technology inspires me to stay abreast of the latest trends and advancements. A commitment to lifelong learning ensures that my skills remain cutting-edge.            </div>
+            </div>
         <img className='image-robot' style={imageStyle} ref={ImageMove} src={Mainimage} alt="..." />
       </div>
     </div>
