4b40b37d9e7650d6112568b7f75d360e8b1373f4
chatanyapra <chatanyapra@gmail.com>
2023-11-15 19:52:59 -0800

    change the font family

--- src/components/GreetingPage.jsx
+++ src/components/GreetingPage.jsx
@@ -55,22 +55,22 @@ export default function GreetingPage() {
           <div className="image-robot-container">
             <Lottie className='robot-anim' animationData={robotAnimation} loop={true} />
           </div>
-          <div className='user-name overflow-hidden text-left text-4xl text-gray-50 font-mono z-10'><span className='text-6xl'>Hi,</span><br/>
-                <span className=''>I am {' '} {text}</span>
+          <div className='user-name overflow-hidden text-left text-4xl text-gray-50 font-mono z-10' style={{ fontFamily: "Exo, sans-serif"}}><span className='text-6xl'>Hi,</span><br/>
+                <span>I am {' '} {text}</span>
                 <Cursor cursorColor='white' cursorStyle= {symbol} />
           </div>
       </div>
       <div ref={box2Container} className="box2-container overflow-hidden absolute w-full bg-gray-900" style={{height: '900px', top: '710px'}}>
-          <div className='aboutme-box text-white bg-slate-600 rounded-2xl relative backdrop-opacity-5 z-10 backdrop-invert bg-black/50 py-7 overflow-x-hidden'>
+          <div className='aboutme-box text-white rounded-2xl relative backdrop-opacity-5 z-10 backdrop-invert bg-black/50 py-7 overflow-x-hidden'>
             <i style={gearBox} className="gear-icon absolute fa-solid fa-gear"></i>
-            <h1 className='text-3xl mx-12 my-6'>About me</h1>
+            <h1 className='text-3xl mx-12 my-6' style={{ fontFamily: "Exo, sans-serif"}}>About me</h1>
             <div className='line-main absolute bg-teal-300'>
               <div className='scanner relative bg-white'></div>
             </div>
             <div className='m-auto text-xl w-10/12'>
               Hello, I'm Chatanya Pratap, a passionate and versatile web developer with expertise in a diverse range of technologies. With a foundation built on HTML, CSS, and JavaScript, I navigate seamlessly across the realms of Laravel, PHP, and React.js, breathing life into innovative and user-centric web applications.
             </div>
-            <h1 className='text-2xl mx-12 mt-10 mb-4'>My Journey</h1>
+            <h1 className='text-2xl mx-12 mt-10 mb-4'  style={{ fontFamily: "Exo, sans-serif"}}>What Sets Me Apart</h1>
             <div className='m-auto text-xl w-10/12'>
               <b>Innovative Solutions: </b>I approach challenges with a mindset that values innovation. Each project is an opportunity to create something unique and impactful, and I thrive on pushing the boundaries of what's possible.  
             </div>
