319da6d9b394acd9a108caeefc1c1fe68736f300
chatanyapra <chatanyapra@gmail.com>
2023-11-17 00:47:17 -0800

    change in contact file

--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -8,9 +8,9 @@ export default function GetInTouch() {
             <h3 className='text-white py-4'>©2023 Designs by Chatanya Pratap. All right reserved.</h3>
         </div>
         <div className="w-52 flex justify-around py-9">
-            <a href="#" target="_blank"><i class="fab fa-instagram text-white text-4xl"></i></a>
-            <a href="#" target="_blank"><i class="fab fa-linkedin text-white text-4xl"></i></a>
-            <a href="#" target="_blank"><i class="fab fa-github text-white text-4xl"></i></a>
+            <a href="#" target="_blank"><i className="fab fa-instagram text-white text-4xl"></i></a>
+            <a href="#" target="_blank"><i className="fab fa-linkedin text-white text-4xl"></i></a>
+            <a href="#" target="_blank"><i className="fab fa-github text-white text-4xl"></i></a>
         </div>
     </div>
   )
