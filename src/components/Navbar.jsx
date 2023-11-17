33229770826b38dfd588829cf65fa18c4575fe85
chatanyapra <chatanyapra@gmail.com>
2023-11-17 00:47:29 -0800

    new navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,9 +6,10 @@ export default function Navbar() {
     <div className='navbar-main w-screen bg-gray-900 h-16 text-white flex justify-between z-10'>
       <div className='navbar_logo w-40 h-full relative text-center pt-4'>Navbar <div className="triangle"></div></div>
       <div className='navbtns mr-5 flex justify-between '>
-        <button className='nav-btn mx-4 my-auto'>Hover me</button>
-        <button className='nav-btn mx-4 my-auto'>Hover me</button>
-        <button className='nav-btn mx-4 my-auto'>Hover me</button>
+        <button className='nav-btn mx-4 my-auto'>Home</button>
+        <button className='nav-btn mx-4 my-auto'>About </button>
+        <button className='nav-btn mx-4 my-auto'>Contact</button>
+        <button className='nav-btn mx-4 my-auto'>Resume</button>
       </div>
     </div>
   )
