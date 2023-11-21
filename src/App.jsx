137e2eff614a7ef8d084c784c9129a11dabab10b
chatanyapra <chatanyapra@gmail.com>
2023-11-21 07:40:00 -0800

    change the font sizes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import './App.css'
 import GetInTouch from './components/GetInTouch';
 import GreetingPage from './components/GreetingPage'
 import Navbar from './components/Navbar'
-import SvgLine from "./components/SvgLine";
 import WorkPage from './components/WorkPage';
+import './components/GreetingPage.css'
+import './components/Navbar.css'
+import './components/WorkPage.css'
 
 function App() {
 
@@ -13,7 +15,7 @@ function App() {
       <Navbar></Navbar>
       <GreetingPage></GreetingPage>
       <div className='w-full justify-evenly flex flex-wrap relative m-auto p-5 pt-60 sm:pt-40 bg-gray-900'>
-        <h1 className='absolute top-40 sm:top-20'  style={{ fontFamily: "Exo, sans-serif"}}>My Work</h1>
+        <h1 className='absolute top-40 sm:top-20 text-7xl text-white'  style={{ fontFamily: "Exo, sans-serif"}}>My Work</h1>
         <WorkPage></WorkPage>
         <WorkPage></WorkPage>
         <WorkPage></WorkPage>
