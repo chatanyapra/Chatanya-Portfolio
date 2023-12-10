9f636e4266f4b775278101a9d00eedfc9e1f33bf
chatanyapra <chatanyapra@gmail.com>
2023-12-09 23:04:17 -0800

    added small change in ui

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import Navbar from './components/Navbar'
 import { Router, BrowserRouter,  Routes, Route } from "react-router-dom";
 import AboutMe from './components/AboutMe.jsx'
 import AboutSkills from './components/AboutSkills.jsx'
+import GetInTouch from './components/GetInTouch.jsx'
+import ContactMe from './components/ContactMe.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <div className='web-container h-screen'>
@@ -13,14 +15,18 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Navbar></Navbar>
       {/* <App/> */}
         <Routes>
-            <Route
-              exact
-              path="/about"
-              element={<> <AboutSkills/> <AboutMe/> </>}></Route>
-            <Route
-              exact
-              path="/"
-              element={<App/>}></Route>
+          <Route
+            exact
+            path="/"
+            element={<App/>}></Route>
+          <Route
+            exact
+            path="/about"
+            element={<> <AboutSkills/> <AboutMe/> </>}></Route>
+          <Route
+            exact
+            path="/contact"
+            element={<> <ContactMe/> <GetInTouch/> </>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
